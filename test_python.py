2712a975973024763fc127721f9b2774f21ea7d0
delphineuni <phintrainer@gmail.com>
2024-04-18 15:23:33 +0200

    changed print text

--- test_python.py
+++ test_python.py
@@ -4,12 +4,15 @@ import Password_Generator as passgen
 class TestSum(unittest.TestCase):
     
     def test_password(self):
-        pwlength = [3,4,5,6,7,9,"a"]
-        passgen.generatePassword(pwlength)
+        try:
+            pwlength = [3,4,5,6,7,9,"a"]
+            passgen.generatePassword(pwlength)
+        except Exception as e:
+            print(e)
     def test_main(self):
         try:
             passgen.main()
         except EOFError:
-            print("Expected EOF error with input")
+            print("\nEOF error is expected with empty input in GitHub Action")
         except Exception as e:
             print(e)
