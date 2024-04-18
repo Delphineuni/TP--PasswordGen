8945829b79370f616189406d3e74b2887d57651a
delphineuni <phintrainer@gmail.com>
2024-04-18 15:13:05 +0200

    updates

---
+++ test_python.py
@@ -0,0 +1,8 @@
+﻿import unittest, os
+import Password_Generator as passgen
+
+class TestSum(unittest.TestCase):
+    
+    def test_password(self):
+        pwlength = [3,4,5,6,7,9,"a"]
+        passgen.generatePassword(pwlength)
