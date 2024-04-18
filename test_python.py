99dcfe9f79299f3fe2094e771ab7b725739b3a9b
delphineuni <phintrainer@gmail.com>
2024-04-18 15:19:49 +0200

    test main

--- test_python.py
+++ test_python.py
@@ -6,3 +6,10 @@ class TestSum(unittest.TestCase):
     def test_password(self):
         pwlength = [3,4,5,6,7,9,"a"]
         passgen.generatePassword(pwlength)
+    def test_main(self):
+        try:
+            passgen.main()
+        except EOFError:
+            print("Expected EOF error with input")
+        except Exception as e:
+            print(e)
