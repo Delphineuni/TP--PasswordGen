5464e738a606779ff7fd4768a8466f0a4aad97ca
delphineuni <phintrainer@gmail.com>
2024-04-18 15:39:05 +0200

    mod line jump

--- test_python.py
+++ test_python.py
@@ -5,9 +5,9 @@ RED = "\033[1;31;40m"
 RESET = "\033[0m"
 
 class TestSum(unittest.TestCase):
-    print(RED,"\n-------------------------------------------------------")
-    print(RED,"------- Generated errors will be printed in red -------")
-    print(RED,"-------------------------------------------------------",RESET)
+    print("\n",RED,"-------------------------------------------------------")
+    print(RED," ------- Generated errors will be printed in red -------")
+    print(RED," -------------------------------------------------------",RESET)
     def test_password(self):
         try:
             pwlength = [3,4,5,6,7,9,"a"]
