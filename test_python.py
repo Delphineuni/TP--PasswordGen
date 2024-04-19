507eef5d497335275372ee801acb4f633d7b4943
delphineuni <phintrainer@gmail.com>
2024-04-19 11:39:56 +0200

    letting GHA fail on purpose with Pytest without catching exceptions

--- test_python.py
+++ test_python.py
@@ -6,33 +6,18 @@ BLUE = "\033[34;40m"
 RESET = "\033[0m"
 
 class TestSum(unittest.TestCase):
-    print("\n",RED,"-------------------------------------------------------")
-    print(RED," ------- Generated errors will be printed in red -------")
-    print(RED," -------------------------------------------------------",RESET)
-    def tests(self):
-        #try:
+    def test_string(self):
         print(BLUE,"Testing string:")
         pwlength = [3,"a"]
+        passgen.generatePassword(pwlength)   
+    def test_float(self):
+        print(BLUE,"Testing float:")
+        pwlength = [3,3.1]
         passgen.generatePassword(pwlength)
-        #except Exception as e:
-        #    print(RED,"Value",BLUE,pwlength[1],RED,"raised error",e,RESET)       
-        try:
-            print(BLUE,"Testing float:")
-            pwlength = [3,3.1]
-            passgen.generatePassword(pwlength)
-        except Exception as e:
-            print(RED,"Value",BLUE,pwlength[1],RED,"raised error",e,RESET)
-        try:
-            print(BLUE,"Testing Integer:")
-            pwlength = [3,4]
-            passgen.generatePassword(pwlength)
-        except Exception as e:
-            print(RED,"Value",BLUE,pwlength[1],RED,"raised error",e,RESET)            
+    def test_int(self):
+        print(BLUE,"Testing Integer:")
+        pwlength = [3,4]
+        passgen.generatePassword(pwlength)          
     def test_main(self):
-        try:
-            print(BLUE,"Testing __main__ without input",RESET)
-            passgen.main()
-        except EOFError:
-            print("\n",RED," EOF error is expected with empty input in GitHub Action")
-        except Exception as e:
-            print(e)
+        print(BLUE,"Testing __main__ without input",RESET)
+        passgen.main()
