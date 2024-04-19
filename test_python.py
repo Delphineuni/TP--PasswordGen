97010857bf31cb4b6ca36d29f9c07942c776d588
delphineuni <phintrainer@gmail.com>
2024-04-19 10:32:16 +0200

    conforming to pytest

--- test_python.py
+++ test_python.py
@@ -1,4 +1,4 @@
-﻿import unittest, os
+﻿import unittest
 import Password_Generator as passgen
 
 RED = "\033[1;31;40m"
@@ -6,25 +6,34 @@ BLUE = "\033[1;34;40m"
 RESET = "\033[0m"
 
 class TestSum(unittest.TestCase):
-    print("\n",RED,"-------------------------------------------------------")
-    print(RED," ------- Generated errors will be printed in red -------")
-    print(RED," -------------------------------------------------------",RESET)
-    def test_password(self):
-        pwlength = [3,4,5,6,7,9,"a",3.1]
-        exceptions = {}
-        for pw in pwlength:
-                try:
-                    pws=[pw,3]
-                    passgen.generatePassword(pws)
-                except Exception as e:
-                    exceptions[pw] = e
-        print('')
-        for exception in exceptions:
-            print(RED,"Value",BLUE,exception,RED,"raised error",exceptions[exception],RESET)
+    #print("\n",RED,"-------------------------------------------------------")
+    #print(RED," ------- Generated errors will be printed in red -------")
+    #print(RED," -------------------------------------------------------",RESET)
+    def test_string(self):
+        pwlength = [3,"a"]
+        passgen.generatePassword(pwlength)
+    def test_float(self):
+        pwlength = [3,3.1]
+        passgen.generatePassword(pwlength)
+    def test_integer(self):
+        pwlength = [3,4]
+        passgen.generatePassword(pwlength)
+    #def test_password(self):
+    #    pwlength = [3,4,5,6,7,9,"a",3.1]
+    #    exceptions = {}
+    #    for pw in pwlength:
+    #            try:
+    #                pws=[pw,3]
+    #                passgen.generatePassword(pws)
+    #            except Exception as e:
+    #                exceptions[pw] = e
+    #    print('')
+    #    for exception in exceptions:
+    #        print(RED,"Value",BLUE,exception,RED,"raised error",exceptions[exception],RESET)
     def test_main(self):
-        try:
+    #    try:
             passgen.main()
-        except EOFError:
-            print("\n",RED," EOF error is expected with empty input in GitHub Action")
-        except Exception as e:
-            print(e)
+    #    except EOFError:
+    #        print("\n",RED," EOF error is expected with empty input in GitHub Action")
+    #    except Exception as e:
+    #        print(e)
