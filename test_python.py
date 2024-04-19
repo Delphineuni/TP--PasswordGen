952e97e202b9a59872d180a3af328fd98299056f
delphineuni <phintrainer@gmail.com>
2024-04-19 11:01:50 +0200

    rewrote test script

--- test_python.py
+++ test_python.py
@@ -6,32 +6,31 @@ BLUE = "\033[1;34;40m"
 RESET = "\033[0m"
 
 class TestSum(unittest.TestCase):
-    #print("\n",RED,"-------------------------------------------------------")
-    #print(RED," ------- Generated errors will be printed in red -------")
-    #print(RED," -------------------------------------------------------",RESET)
-    def test_string(self):
-        pwlength = [3,"a"]
-        passgen.generatePassword(pwlength)
-    def test_float(self):
-        pwlength = [3,3.1]
-        passgen.generatePassword(pwlength)
-    def test_integer(self):
-        pwlength = [3,4]
-        passgen.generatePassword(pwlength)
-    #def test_password(self):
-    #    pwlength = [3,4,5,6,7,9,"a",3.1]
-    #    exceptions = {}
-    #    for pw in pwlength:
-    #            try:
-    #                pws=[pw,3]
-    #                passgen.generatePassword(pws)
-    #            except Exception as e:
-    #                exceptions[pw] = e
-    #    print('')
-    #    for exception in exceptions:
-    #        print(RED,"Value",BLUE,exception,RED,"raised error",exceptions[exception],RESET)
+    print("\n",RED,"-------------------------------------------------------")
+    print(RED," ------- Generated errors will be printed in red -------")
+    print(RED," -------------------------------------------------------",RESET)
+    def tests(self):
+        try:
+            print(BLUE,"Testing string:")
+            pwlength = [3,"a"]
+            passgen.generatePassword(pwlength)
+        except Exception as e:
+            print(RED,"Value",BLUE,pwlength[1],RED,"raised error",e,RESET)       
+        try:
+            print(BLUE,"Testing float:")
+            pwlength = [3,3.1]
+            passgen.generatePassword(pwlength)
+        except Exception as e:
+            print(RED,"Value",BLUE,pwlength[1],RED,"raised error",e,RESET)
+        try:
+            print(BLUE,"Testing Integer:")
+            pwlength = [3,4]
+            passgen.generatePassword(pwlength)
+        except Exception as e:
+            print(RED,"Value",BLUE,pwlength[1],RED,"raised error",e,RESET)            
     def test_main(self):
         try:
+            print(BLUE,"Testing __main__ without input",RESET)
             passgen.main()
         except EOFError:
             print("\n",RED," EOF error is expected with empty input in GitHub Action")
